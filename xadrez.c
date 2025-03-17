35d1e5e645af10148649a19b1577427a97ecb311
Novaess001 <henriquenovaes1589@gmail.com>
2025-03-17 14:35:16 +0000

    mestre concluido

--- xadrez.c
+++ xadrez.c
@@ -1,44 +1,72 @@
 #include <stdio.h>
  
-int main() {
-    
-    int i, moviesquerda = 1;
-
-
-      printf("Bem Vindo ao Jogo Xadrez\n");
-
-      printf("\nBispo andar 5 casas para Diagonal \n ");
-      for (i = 1; i <= 5; i++){
-         printf("Direita Cima\n");
-
+   void bispo(){
+      for (int i = 0; i <= 5; i++){
+         printf("Cima \n");
       }
-          
-      i = 1;
 
-      printf("\nRainha andar 8 casas para Esquerda \n");
-        while (i <= 8) {
-            printf("Esquerda\n");
-            i++;
-        }
+          printf("\n");
+
+            for (int j = 0; j <= 5; j++){  
+               printf("Direita\n ");
+               
+            }
+            
+   }  
+
+   void torre(int movitorre){
+      if (movitorre > 0){
+         printf("Direita \n");
+         torre(movitorre - 1);
+      }
+      
+   }
+
+   void rainha(int movirainha){
+      if (movirainha > 0){
+         printf("Esquerda\n");
+         rainha(movirainha - 1);
+      }
+      
+   }
+
+
+int main(){
+
+   printf("\nBem Vindo ao Jogo Xadrez\n");
+   printf("Bispo andar 5 casas para Diagonal\n ");
+   bispo();
+
+   printf("\nTorre andar 5 casas para Direita \n");
+   torre(5);
+
+   printf("\nRainha andar 8 casas para Esquerda \n");
+   rainha(8);
+
+   printf("\nCavalo andar 2 casas para Cima, e 1 para Direita\n");
+   for (int i = 0; i <= 1; i++){
+      if (i == 0) {
+        printf("Cima\n");
+        continue;  
+    }
    
-       i = 1;
+    for (int j = 0; j <= 1; j++){
+        if (i == 1 && j == 0) {
+        printf("Cima \n");
+        break;
+        }
+    }
 
-       printf("\nTorre andar 5 casas para frente \n");
-       do
-       {
-         printf("Frente \n");
-         i++;
-       } while (i <= 5);
-       
-       printf("\nCavalo andar 2 casas para baixo, e 1 para esquerda\n");
-       while (moviesquerda--)
-    {
-         for (i = 0; i <= 1; i++){
-            printf("Baixo \n");
+    if(i == 1){
+        printf("Direita \n");
+    }
 
-         }
-         printf("Esquerda \n");
-      }
-
-    return 0;
+    
+   printf("\n");
 }
+
+   return 0;
+
+}
+    
+    
